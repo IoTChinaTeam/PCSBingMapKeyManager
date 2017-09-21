01a6d036a34bd3b2f53f88d29bbfac2c3b82b7e2
Xiangzhi Sheng <xisheng@microsoft.com>
2017-09-21 15:51:46 +0800

    Initial commit

---
+++ BingMapKeyManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PCSBingMapKeyManager
+{
+    internal class BingMapKeyManager
+    {
+        private const string urlKey = "CosmosDb:url";
+        private const string keyKey = "CosmosDb:key";
+        private const string databaseIdKey = "CosmosDb:databaseId";
+        private const string collectionIdKey = "CosmosDb:collectionId";
+        private const string documentIdKey = "CosmosDb:documentId";
+        private const string BingMapKeyKey = "BingMapKey";
+
+        private readonly DocumentClient documentClient;
+        private readonly string databaseId;
+        private readonly string collectionId;
+        private readonly string documentId;
+        private readonly string logicCollectionId;
+        private readonly string logicKey;
+        private readonly string collectionLink;
+        private readonly string documentLink;
+
+        public BingMapKeyManager(IConfigurationRoot configuration)
+        {
+            var url = configuration.GetValue<string>(urlKey);
+            var key = configuration.GetValue<string>(keyKey);
+            databaseId = configuration.GetValue<string>(databaseIdKey);
+            collectionId = configuration.GetValue<string>(collectionIdKey);
+            documentId = configuration.GetValue<string>(documentIdKey);
+
+            var parts = documentId.Split('.');
+            if (parts.Length != 2)
+            {
+                throw new InvalidConfigurationException($"Incorrect configuration {documentIdKey} = {documentId}. It must be in form <collection>.<key>");
+            }
+
+            logicCollectionId = parts[0];
+            logicKey = parts[1];
+
+            collectionLink = $"/dbs/{databaseId}/colls/{collectionId}";
+            documentLink = $"{collectionLink}/docs/{documentId}";
+
+            documentClient = new DocumentClient(new Uri(url), key);
+        }
+
+        public async Task<string> GetAsync()
+        {
+            var root = await ReadDocumentAsync();
+
+            return root?[BingMapKeyKey]?.ToString();
+        }
+
+        public async Task<bool> SetAsync(string key)
+        {
+            var root = await ReadDocumentAsync() ?? new JObject();
+
+            root[BingMapKeyKey] = key;
+            return await WriteDocumentAsync(root);
+        }
+
+        private async Task<JToken> ReadDocumentAsync()
+        {
+            try
+            {
+                var response = await documentClient.ReadDocumentAsync<ThemeDocument>(documentLink);
+                return JsonConvert.DeserializeObject(response.Document.Data) as JToken;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private async Task<bool> WriteDocumentAsync(JToken root)
+        {
+            try
+            {
+                await documentClient.ReadDocumentCollectionAsync(collectionLink);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Failed to set the key (collection is not available): {ex.Message}");
+                return false;
+            }
+
+            var document = new ThemeDocument
+            {
+                Id = $"{documentId}",
+                CollectionId = logicCollectionId,
+                Key = logicKey,
+                Data = JsonConvert.SerializeObject(root)
+            };
+
+            try
+            {
+                await documentClient.UpsertDocumentAsync(collectionLink, document);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Failed to set the key: {ex.Message}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace PCSBingMapKeyManager
+{
+    class Program
+    {
+        private const string NewKeySwitch = "/a:";
+
+        static void Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddIniFile("appsettings.ini");
+            var configuration = configurationBuilder.Build();
+
+            if (args.Length > 1 || args.Any() && !args[0].StartsWith(NewKeySwitch))
+            {
+                Usage();
+                return;
+            }
+
+            var manager = new BingMapKeyManager(configuration);
+
+            if (!args.Any())
+            {
+                var key = manager.GetAsync().Result;
+                if (string.IsNullOrEmpty(key))
+                {
+                    Console.WriteLine("No bing map key set");
+                }
+                else
+                {
+                    Console.WriteLine($"Bing map key = {key}");
+                }
+
+                Console.WriteLine($"\nHint: Use {NewKeySwitch}<key> to set or update the key");
+            }
+            else
+            {
+                var key = args[0].Substring(NewKeySwitch.Length);
+                if (manager.SetAsync(key).Result)
+                {
+                    Console.WriteLine($"Bing map key set as '{key}'");
+                }
+            }
+        }
+
+        private static void Usage()
+        {
+            var entry = Path.GetFileName(Assembly.GetEntryAssembly().CodeBase);
+            Console.WriteLine($"Usage: {entry} [{NewKeySwitch}<new key>]");
+        }
+    }
+}
